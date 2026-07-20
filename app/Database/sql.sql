8b86742ae39ea6ac9cf2a81a67b5fdfea7d47e47
Giovan2005 <mamihajagiovan2005@gmail.com>
2026-07-20 09:13:56 +0300

    craetion table

--- app/Database/sql.sql
+++ app/Database/sql.sql
@@ -1,44 +1,120 @@
--- Table Produit
-CREATE TABLE produit (
+PRAGMA foreign_keys = ON;
+
+
+CREATE TABLE operateur (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    designation TEXT NOT NULL,
-    prix REAL NOT NULL,
-    quantite_stock INTEGER NOT NULL
+    nom TEXT NOT NULL UNIQUE
 );
 
--- Table Caisse
-CREATE TABLE caisse (
+CREATE TABLE prefixe (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    numero TEXT NOT NULL,
-    montant REAL DEFAULT 0
+    id_operateur INTEGER NOT NULL,
+    code TEXT NOT NULL UNIQUE,
+    FOREIGN KEY (id_operateur) REFERENCES operateur(id)
 );
 
--- Table Achat
-CREATE TABLE achat (
+CREATE TABLE client (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    produit_id INTEGER NOT NULL,
-    caisse_id INTEGER NOT NULL,
-    quantite INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    date_achat DATETIME DEFAULT CURRENT_TIMESTAMP,
-
-    FOREIGN KEY (produit_id) REFERENCES produit(id),
-    FOREIGN KEY (caisse_id) REFERENCES caisse(id),
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    nom TEXT NOT NULL,
+    telephone TEXT NOT NULL UNIQUE
 );
 
-CREATE TABLE mouvement_stock (
+CREATE TABLE type_operation (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    produit_id INTEGER NOT NULL,
-    type_mouvement TEXT NOT NULL, -- ENTREE ou SORTIE
-    quantite INTEGER NOT NULL,
+    nom TEXT NOT NULL UNIQUE
+);
+CREATE TABLE bareme_frais (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_type_operation INTEGER NOT NULL,
+    montant_min INTEGER NOT NULL,
+    montant_max INTEGER NOT NULL,
+    frais INTEGER NOT NULL,
+    FOREIGN KEY (id_type_operation) REFERENCES type_operation(id)
+);
+
+CREATE TABLE operation (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_type_operation INTEGER NOT NULL,
+    client_source INTEGER,
+    client_destination INTEGER,
+    montant INTEGER NOT NULL,
+    frais INTEGER NOT NULL DEFAULT 0,
+    date_operation DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_type_operation) REFERENCES type_operation(id),
+    FOREIGN KEY (client_source) REFERENCES client(id),
+    FOREIGN KEY (client_destination) REFERENCES client(id)
+);
+
+
+CREATE TABLE type_mouvement (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom TEXT NOT NULL UNIQUE
+);
+
+
+CREATE TABLE mouvement (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_operation INTEGER NOT NULL,
+    id_type_mouvement INTEGER NOT NULL,
+    montant INTEGER NOT NULL,
     date_mouvement DATETIME DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (produit_id) REFERENCES produit(id)
+    FOREIGN KEY (id_operation) REFERENCES operation(id),
+    FOREIGN KEY (id_type_mouvement) REFERENCES type_mouvement(id)
 );
 
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-);
+
+INSERT INTO operateur(nom) VALUES
+('Orange Money'),
+('MVola'),
+('Airtel Money');
+
+
+INSERT INTO prefixe(id_operateur, code) VALUES
+(1, '032'),
+(1, '037'),
+(2, '034'),
+(2, '038'),
+(3, '033');
+
+
+INSERT INTO type_operation(nom) VALUES
+('Depot'),
+('Retrait'),
+('Transfert');
+
+
+INSERT INTO type_mouvement(nom) VALUES
+('Debit'),
+('Credit');
+
+-- Dépôt (gratuit)
+INSERT INTO bareme_frais(id_type_operation, montant_min, montant_max, frais) VALUES
+(1, 100, 999999999, 0);
+
+-- Retrait (moins cher)
+INSERT INTO bareme_frais(id_type_operation, montant_min, montant_max, frais) VALUES
+(2, 100, 1000, 25),
+(2, 1001, 5000, 50),
+(2, 5001, 10000, 75),
+(2, 10001, 25000, 150),
+(2, 25001, 50000, 300),
+(2, 50001, 100000, 600),
+(2, 100001, 250000, 1000),
+(2, 250001, 500000, 1200),
+(2, 500001, 1000000, 1800),
+(2, 1000001, 2000000, 2500);
+
+-- Transfert (plus cher)
+INSERT INTO bareme_frais(id_type_operation, montant_min, montant_max, frais) VALUES
+(3, 100, 1000, 50),
+(3, 1001, 5000, 50),
+(3, 5001, 10000, 100),
+(3, 10001, 25000, 200),
+(3, 25001, 50000, 400),
+(3, 50001, 100000, 800),
+(3, 100001, 250000, 1500),
+(3, 250001, 500000, 1500),
+(3, 500001, 1000000, 2500),
+(3, 1000001, 2000000, 3000);
